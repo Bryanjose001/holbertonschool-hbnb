24899b84db7e8fb6442feafc4e5eba2dc0e71210
Bryanjose001 <bryan.jose2541@gmail.com>
2025-06-25 15:35:22 -0400

    part 2

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,11 +1,12 @@
 from app.models.base_entity import BaseModel
 
+
 class User(BaseModel):
  def __init__(self, first_name: str, last_name: str, email: str, is_admin: bool = False):
         super().__init__()
-        self.first_name = self.validate_name(first_name, "First name")
-        self.last_name = self.validate_name(last_name, "Last name")
-        self.email = self.validate_email(email)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.is_admin = is_admin
 
 def validate_name(self, name: str, field: str):
@@ -16,7 +17,6 @@ def validate_name(self, name: str, field: str):
 def validate_email(self, email: str):
     if not email or not isinstance(email, str) or "@" not in email:
         raise ValueError("A valid email address is required")
-        return email.strip().lower()
 
 def register(self):
     print(f"Registering user: {self.email}")
