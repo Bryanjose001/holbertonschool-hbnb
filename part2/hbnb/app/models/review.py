62c9773b098520002679824628421a0a4288e4ea
Bryanjose001 <bryan.jose2541@gmail.com>
2025-06-25 17:52:37 -0400

    part 2

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,6 +1,4 @@
 from app.models.base_entity import BaseModel
-from app.models.place import Place
-from app.models.user import User
 
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
@@ -36,25 +34,3 @@ class Review(BaseModel):
         if not isinstance(value, (int, float)) or not (1 <= value <= 5):
             raise ValueError("Rating must be a number between 1 and 5")
         self._rating = float(value)
-
-    # --- Place ---
-    @property
-    def place(self):
-        return self._place
-
-    @place.setter
-    def place(self, value):
-        if not isinstance(value, Place):
-            raise ValueError("Place must be a valid Place instance")
-        self._place = value
-
-    # --- User ---
-    @property
-    def user(self):
-        return self._user
-
-    @user.setter
-    def user(self, value):
-        if not isinstance(value, User):
-            raise ValueError("User must be a valid User instance")
-        self._user = value
